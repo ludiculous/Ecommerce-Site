7f6d064125644450971e73579b8f7dcb74942713
Preston <ludiculous89@gmail.com>
2016-01-29 19:53:38 -0800

    first

---
+++ config/secret.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  database:'mongodb://ludiculous:123456@ds049935.mongolab.com:49935/gogrin',
+  port: 3000,
+  secretKey: "PHapdfipd23@#"
+}
+
+//mongodb://localhost/gogrin
+//
